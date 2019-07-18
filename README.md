1ce465d419cdaa5089a7f4f6d54801290908cbff
Joyce Buettner <iamtechiegirl@cox.net>
2019-07-17 20:58:20 -0700

    app is now working

--- README.md
+++ README.md
@@ -1,2 +1,65 @@
 # liri-node-app
 Homework-LIRI
+# LIRI Bot
+
+## Assignment
+
+Create a CLI App "LIRI" bot which stands for Language Interpretation and Recognition Interface. LIRI will be a command line node app that takes in parameters and gives you back data.
+
+## Solution
+
+The solution was to use "backend" technologies only.  Node.js was used along with NPM (Node Package Manager) libraries in order to accomplish the tasks. There is no front end to this application, therefore this is no html page to publish.
+
+## Technologies
+JavaScript, Node.js, Request library, Spotify API, dateFormat library, File system library.
+
+## Using LIRI
+
+![LIRI Command Examples](screenshots/commands.jpg?raw=true "LIRI Command Examples")
+
+### `concert-this`
+
+  `node liri.js concert-this <artist/band name>`
+
+   * This will search the Bands in Town Artist Events API for an artist and show the following information about each event in the terminal and to the log.txt file:
+
+    ```
+      * Name of the venue
+      * Venue location
+      * Date of the Event 
+    ```
+
+### `spotify-this-song`
+
+  `node liri.js spotify-this-song '<song name>'`
+
+   * This will search the Spotify API for a song and show the following information about the song in the `terminal` and to the `log.txt` file:
+   
+    ```
+      * Artist(s)
+      * The song's name
+      * A preview link of the song from Spotify
+      * The album that the song is from
+    ```
+
+### `movie-this`
+
+  `node liri.js movie-this '<movie name here>'`
+
+  * This will search the OMDB API for a movie name and show the following information in the `terminal` and to the `log.txt` file:
+
+    ```
+      * Title of the movie.
+      * Year the movie came out.
+      * IMDB Rating of the movie.
+      * Rotten Tomatoes Rating of the movie.
+      * Country where the movie was produced.
+      * Language of the movie.
+      * Plot of the movie.
+      * Actors in the movie.
+    ```
+
+### `do-what-it-says`
+  `node liri.js do-what-it-says`
+
+   * LIRI will take the text inside of `random.txt` and then use it to call one of LIRI's commands.
--- liri.js
+++ liri.js
@@ -7,33 +7,40 @@ var fs = require("fs");
 var moment = require('moment');
 moment().format();
 
+var command = process.argv[2];
+var arg2 = process.argv[3];
+
 startProg(command, arg2);
 
-function startProg(command,arg2){
-    switch(command){
-        case "concert-this": concertThis(arg2);
-                                break;
-        case "movie-this" : movieThis(arg2);
-                                break;
-        case "spotify-this": spotify(arg2);
-                                break;
-        case "do-what-it-says": doWhatItSays();
-                                break;
-        default:  console.log("Liri doesn't know what you're saying");
-        
+function startProg(command, arg2) {
+    switch (command) {
+        case "concert-this":
+            concertThis(arg2);
+            break;
+        case "movie-this":
+            movieThis(arg2);
+            break;
+        case "spotify-this":
+            spotify(arg2);
+            break;
+        case "do-what-it-says":
+            doWhatItSays();
+            break;
+        default: console.log("Liri doesn't know what you're saying");
+
     }
 }
 
 //1.node liri.js concert-this "artist/band name"
 //render *name of venue/*venue location/*date of event - moment "MM/DD/YYYY"
 
-function concertThis(artist){
-  
-    var queryURL= "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
-    axios.get(queryURL).then(function(res){
-        var result  =  res.data;
+function concertThis(artist) {
+
+    var queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
+    axios.get(queryURL).then(function (res) {
+        var result = res.data;
         console.log(result);
-        for(var i=0; i<result.length; i++){
+        for (var i = 0; i < result.length; i++) {
             var show = result[i];
             console.log(show.venue.country);
             console.log(show.venue.city);
@@ -48,48 +55,48 @@ function concertThis(artist){
 // render *Artist(s)/*The song name/*A preview link of the song from Spotify/*The album that the song is from.
 // * If no song is provided then your program will default to "The Sign" by Ace of Base.
 
-var spotifyThis = function(song) {
-    if(song === undefined){
+var spotify = function (song) {
+    if (song === undefined) {
         song = "The Sign";
     }
-}
-  
-    var queryURL= "https://www.npmjs.com/package/node-spotify-api" + song + "/events?app_id=41acbdee36f4460cb411061ccab2a65f"
-    axios.get(queryURL).then(function(res){
-        var result  =  res.data;
+
+
+    var queryURL = "https://www.npmjs.com/package/node-spotify-api" + song + "app_id=41acbdee36f4460cb411061ccab2a65f"
+    axios.get(queryURL).then(function (res) {
+        var result = res.data;
         console.log(result);
-        for(var i=0; i<result.length; i++){
+        for (var i = 0; i < result.length; i++) {
             var show = result[i];
             console.log("Artist: " + songData.artists[0].name);
             console.log("Song: " + songData.name);
             console.log("Preview URL: " + songData.preview_url);
             console.log("Album: " + songData.album.name);
         }
-    })
 
+    })
+}
 
 //3. node liri.js movie-this "movie name"
 // *render *Title of the movie./ *Year movie came out./* IMDB Rating/*Rotten Tomatoes Rating/* Country produced./*Language/*Plot/*Actors.
 // * If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.'
 
-var command = process.argv[2];
-var arg2 = process.argv[3];
 
-var movieThis = function(movie){
-    if(movie === undefined){
+function movieThis(movie) {
+
+    if (movie === undefined) {
         movie = "Mr Nobody";
     }
-    var queryURL= "http://www.omdbapi.com/?t=" + movie + "&y=&plot=full&tomatoes=true&apikey=trilogy";
-    axios.get(queryURL).then(function(res){
-        var result  =  res.data;
-      console.log("Title :" + result.Title);
-      console.log("Year Released :" + result.Released);
-      console.log("IMDB Rating :" + result.imdbRating );
-      console.log("Rotten Tomatoes Rating :" + result.Ratings[0].Value);
-      console.log("Country :" +  result.Country);
-      console.log("Language :" + result.Language);
-      console.log("Plot :" + result.Plot);
-      console.log("Actors :" +  result.Actors);
+    var queryURL = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=full&tomatoes=true&apikey=trilogy";
+    axios.get(queryURL).then(function (res) {
+        var result = res.data;
+        console.log("Title :" + result.Title);
+        console.log("Year Released :" + result.Released);
+        console.log("IMDB Rating :" + result.imdbRating);
+        console.log("Rotten Tomatoes Rating :" + result.Ratings[0].Value);
+        console.log("Country :" + result.Country);
+        console.log("Language :" + result.Language);
+        console.log("Plot :" + result.Plot);
+        console.log("Actors :" + result.Actors);
 
     })
 }
